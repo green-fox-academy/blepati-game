44f1f46de7079010423df8f027505bf5cae6ea7d
Blesa Patricia <blesapati@gmail.com>
2017-04-11 18:05:18 +0200

    fix(game-basics)refactor the class and now the map draw function is working

--- game-basics.py
+++ game-basics.py
@@ -1,5 +1,9 @@
 from tkinter import *
 
+root = Tk()
+canvas = Canvas(root, width=720, height=720, background = "black")
+canvas.pack()
+
 map1 = [
       [0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -13,33 +17,20 @@ map1 = [
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       ]
 
-root = Tk()
-canvas = Canvas(root, width=720, height=720, background = "black")
-canvas.pack()
-
-
-class FloorTile(object):
-    def __init__(self, x = 0, y = 0, walk_on = True):
-        self.x = x
-        self.y = y
-        self.walk_on = walk_on
+class FloorMap(object):
+    def __init__(self):
         self.background_floor = PhotoImage(file="assets/floor.png")
         self.background_wall = PhotoImage(file="assets/wall.png")
 
-    def draw_floor(self):
-        canvas.create_image(self.x, self.y, anchor = "nw", image = self.background_floor)
-    def draw_wall(self):
-        canvas.create_image(self.x, self.y, anchor = "nw", image = self.background_wall)
-
-    def display(self):
+    def display(self): #a valtozokat csinald meg csinosabbra
         for row in range(len(map1)):
             for cell in range(len(map1[row])):
                 if map1[cell][row] == 0:
-                    self.draw_floor()
-                    self.x += 72
-                elif cell == 1:
-                    self.draw_wall()
+                    canvas.create_image(row*72, cell*72, anchor = "nw", image = self.background_floor)
+                else:
+                    canvas.create_image(row*72, cell*72, anchor = "nw", image = self.background_wall)
 
-floor = FloorTile()
+floor = FloorMap()
 floor.display()
+
 root.mainloop()
