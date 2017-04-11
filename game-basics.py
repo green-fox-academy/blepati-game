0421e457a34b3ae5b8ee66efeae05db739635f48
Blesa Patricia <blesapati@gmail.com>
2017-04-11 17:03:54 +0200

    feat(game-basics) add draw floor and wall draw function, not working yet

--- game-basics.py
+++ game-basics.py
@@ -1,20 +1,45 @@
 from tkinter import *
 
+map1 = [
+      [0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ]
+
 root = Tk()
-canvas = Canvas(root, width=600, height=600)
+canvas = Canvas(root, width=720, height=720, background = "black")
 canvas.pack()
 
+
 class FloorTile(object):
-    def __init__(self, x = 0, y = 0, size = 60, walk_on = True):
+    def __init__(self, x = 0, y = 0, walk_on = True):
         self.x = x
         self.y = y
-        self.size = size
         self.walk_on = walk_on
+        self.background_floor = PhotoImage(file="assets/floor.png")
+        self.background_wall = PhotoImage(file="assets/wall.png")
 
     def draw_floor(self):
-        floor = canvas.create_rectangle(self.x, self.y, self.x + self.size, self.y + self.size, fill='green')
+        canvas.create_image(self.x, self.y, anchor = "nw", image = self.background_floor)
+    def draw_wall(self):
+        canvas.create_image(self.x, self.y, anchor = "nw", image = self.background_wall)
 
+    def display(self):
+        for row in range(len(map1)):
+            for cell in range(len(map1[row])):
+                if map1[cell][row] == 0:
+                    self.draw_floor()
+                    self.x += 72
+                elif cell == 1:
+                    self.draw_wall()
 
 floor = FloorTile()
-floor.draw_floor()
+floor.display()
 root.mainloop()
