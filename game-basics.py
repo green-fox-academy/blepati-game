996ef46177b1cff96aabf3e612516d5cdc78182c
Blesa Patricia <blesapati@gmail.com>
2017-04-12 14:36:45 +0200

    feat(game-basics) good enough test hero class, it's need fixing

--- game-basics.py
+++ game-basics.py
@@ -1,21 +1,8 @@
 from tkinter import *
-
+from boards import *
 root = Tk()
 canvas = Canvas(root, width=720, height=720, background = "black")
 canvas.pack()
-
-map1 = [
-      [0, 0, 1, 1, 0, 0, 1, 0, 0, 0],
-      [0, 0, 1, 0, 0, 0, 1, 0, 1, 1],
-      [0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
-      [1, 0, 1, 1, 1, 0, 1, 1, 0, 0],
-      [0, 0, 0, 0, 1, 0, 0, 1, 0, 1],
-      [1, 1, 0, 0, 1, 0, 0, 1, 0, 0],
-      [0, 0, 0, 0, 1, 0, 1, 1, 0, 0],
-      [0, 0, 0, 0, 1, 0, 1, 0, 0, 1],
-      [1, 1, 1, 0, 0, 0, 0, 0, 1, 1],
-      [0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
-      ]
 
 class FloorMap(object):
     def __init__(self):
@@ -23,24 +10,34 @@ class FloorMap(object):
         self.background_tile_wall = PhotoImage(file="assets/wall.png")
         self.tile = 72
 
-    def map_display(self, map): #a valtozokat csinald meg csinosabbra
-        for row in range(len(map)):
-            for cell in range(len(map[row])):
-                if map[cell][row] == 0:
-                    canvas.create_image(row*self.tile, cell*self.tile, anchor = "nw", image = self.background_tile_floor)
+    def map_display(self, board): #a valtozokat csinald meg csinosabbra
+        for row in range(len(board)):
+            for cell in range(len(board[row])):
+                if board[cell][row] == 0:
+                    canvas.create_image(row*self.tile, cell*self.tile, anchor = NW, image = self.background_tile_floor)
                 else:
-                    canvas.create_image(row*self.tile, cell*self.tile, anchor = "nw", image = self.background_tile_wall)
+                    canvas.create_image(row*self.tile, cell*self.tile, anchor = NW, image = self.background_tile_wall)
 
 class Character(object):
-    def __init__(self, char_pic):
-        self.char_pic = PhotoImage(file="assets/" + char_pic)
+    def __init__(self):
+        #self.char_pic = PhotoImage(file="assets/" + char_pic)
+        self.pos_x = 0
+        self.pos_y = 0
+        #self.tile = 72
 
-    def draw_character(self):
-        canvas.create_image(0, 0, anchor = "nw", image = self.char_pic)
+    def draw_character(self, pos_x, pos_y, char_pic):
+        canvas.create_image(pos_x*72, pos_y*72, anchor = NW, image = char_pic)
+
+class Hero(Character):
+    def __init__(self):
+        self.char_pic_down =  PhotoImage(file="assets/hero-down.png")
+        self.char_pic_up=  PhotoImage(file="assets/hero-up.png")
+        self.char_pic_left =  PhotoImage(file="assets/hero-left.png")
+        self.char_pic_right =  PhotoImage(file="assets/hero-right.png")
+        self.draw_character(3, 4, self.char_pic_right)
 
 floor = FloorMap()
 floor.map_display(map1)
-hero = Character("hero-down.png")
-hero.draw_character()
+hero = Hero()
 
 root.mainloop()
